f95acc60503c46de748701a4ba164255828cf109
Emanuel Jöbstl <emanuel.joebstl@gmail.com>
2016-12-17 02:34:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Android Sensor Logger 
+
+Connects to various sensors of an Android phone. 
+Fetches sensor data as fast as possible.
+Writes the data into a CSV file, including a timestamp.
+
+That's it. 
---
+++ app/src/main/java/io/iam360/sensorlog/MainActivity.java
@@ -0,0 +1,134 @@
+package io.iam360.sensorlog;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener2;
+import android.hardware.SensorManager;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener2 {
+
+    SensorManager manager;
+    Button buttonStart;
+    Button buttonStop;
+    boolean isRunning;
+    final String TAG = "SensorLog";
+    FileWriter writer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        isRunning = false;
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStop = (Button)findViewById(R.id.buttonStop);
+
+        buttonStart.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                buttonStart.setEnabled(false);
+                buttonStop.setEnabled(true);
+
+                Log.d(TAG, "Writing to " + getStorageDir());
+                try {
+                    writer = new FileWriter(new File(getStorageDir(), "sensors.csv"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                manager.registerListener(MainActivity.this, manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 0);
+                manager.registerListener(MainActivity.this, manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), 0);
+                manager.registerListener(MainActivity.this, manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED), 0);
+                manager.registerListener(MainActivity.this, manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), 0);
+                manager.registerListener(MainActivity.this, manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED), 0);
+                manager.registerListener(MainActivity.this, manager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR), 0);
+
+                isRunning = true;
+                return true;
+            }
+        });
+        buttonStop.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                buttonStart.setEnabled(true);
+                buttonStop.setEnabled(false);
+                isRunning = false;
+                manager.flush(MainActivity.this);
+                manager.unregisterListener(MainActivity.this);
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return true;
+            }
+        });
+    }
+
+    private String getStorageDir() {
+        return this.getExternalFilesDir(null).getAbsolutePath();
+      //  return "/storage/emulated/0/Android/data/com.iam360.sensorlog/";
+    }
+
+    @Override
+    public void onFlushCompleted(Sensor sensor) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent evt) {
+        if(isRunning) {
+            try {
+                switch(evt.sensor.getType()) {
+                    case Sensor.TYPE_ACCELEROMETER:
+                        writer.write(String.format("%d; ACC; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
+                        break;
+                    case Sensor.TYPE_GYROSCOPE_UNCALIBRATED:
+                        writer.write(String.format("%d; GYRO_UN; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], evt.values[3], evt.values[4], evt.values[5]));
+                        break;
+                    case Sensor.TYPE_GYROSCOPE:
+                        writer.write(String.format("%d; GYRO; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
+                        break;
+                    case Sensor.TYPE_MAGNETIC_FIELD:
+                        writer.write(String.format("%d; MAG; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
+                        break;
+                    case Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED:
+                        writer.write(String.format("%d; MAG_UN; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], 0.f, 0.f, 0.f));
+                        break;
+                    case Sensor.TYPE_ROTATION_VECTOR:
+                        writer.write(String.format("%d; ROT; %f; %f; %f; %f; %f; %f\n", evt.timestamp, evt.values[0], evt.values[1], evt.values[2], evt.values[3], 0.f, 0.f));
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
